fc8226466a6e5f6a1c2e9ef042077f3649540eee
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2022-12-27 21:00:00 +0800

    Create New companies.sql

---
+++ New companies.sql
@@ -0,0 +1,6 @@
+select c.company_code, c.founder, count( distinct e.lead_manager_code),  count( distinct e.senior_manager_code),  count( distinct e.manager_code), count( distinct e.employee_code)
+from employee e
+
+join company c on e.company_code=c.company_code
+group by c.company_code, c.founder
+order by c.company_code
