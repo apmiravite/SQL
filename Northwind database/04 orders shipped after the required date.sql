68d3ff7c19af02f1c34cf16613338efae53d869f
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2025-10-16 17:48:23 +0800

    Create 04 orders shipped after the required date.sql

---
+++ Northwind database/04 orders shipped after the required date.sql
@@ -0,0 +1,5 @@
+-- Show the employee_id, order_id, customer_id, required_date, shipped_date from all orders shipped later than the required date
+
+SELECT employee_id, order_id, customer_id, required_date, shipped_date
+from orders
+where shipped_date>required_date
